568b8b73599923750961e87e011a9a4c66abff2b
Loknath Reddy <devopsloknath94@gmail.com>
2026-02-03 18:22:15 +0530

    missing some line modfied

--- date.sh
+++ date.sh
@@ -4,6 +4,6 @@ echo "this script is started at: $STARTTIME"
 sleep 10
 ENDTIME=$(date +%s)
 
-TOTALTIME=$(($STARTTIME-$ENDTIME))
+TOTALTIME=$(($ENDTIME-$STARTTIME))
 
 echo "the script total time is: $TOTALTIME"
