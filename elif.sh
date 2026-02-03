448ae42d077e485d6acf2e84ea101599a2e36cb5
Loknath Reddy <devopsloknath94@gmail.com>
2026-02-03 21:46:33 +0530

    missing some line modfied

--- elif.sh
+++ elif.sh
@@ -3,7 +3,7 @@ NUMBER=$1
 
 if [ $NUMBER -gt 20 ];then
 echo "given $NUMBER greater then 20"
-elif [ $NUMBER -eq 20 ]
+elif [ $NUMBER -eq 20 ]; then
 echo "given $NUMBER is equal to 20"
 else 
 echo "given $NUMBER is lesser than 20"
