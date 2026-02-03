41ef2217af3690a5a5bd993022e1a872c3e427c8
Loknath Reddy <devopsloknath94@gmail.com>
2026-02-03 22:12:56 +0530

    missing some line modfied

--- nginx.sh
+++ nginx.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
+STARTTIME=$(date +%A)
+mkdir -p /tmp/backup
+LOGFILE=$/tmp/backup
 ID=$(id -u)
+R="\e[33m"
+G="\e[32m"
+Y="\e[31m"
+N="\e[0m"
 
-if [ $ID -ne 0 ]; then 
-echo -e "please run this script with root user"
-exit
-else 
-yum install nginx -y 
+validate(){
+    if [ $1 -ne 0 ];then 
+    echo -e "$2..$R failed $N"
+    else 
+    echo -e "$2..$G success $N"
+    fi
+}
+
+if [ $ID -ne 0 ];then 
+echo -e " $R..please run this script with root user$N"
+exit 1
 fi
+
+echo -e " $G..installing nginx$N "
+yum install nginx -y 
+
+if [ $? -ne 0 ];then 
+yum install nginx -y 
+validate $? "installing nginx"
+else
+echo -e "nginx is already installed $Y skipping$N"
+fi
+sleep 30
+ENDTIME=$(date +%A)
+TOTALTIME=$(($ENDTIME-$STARTTIME))
+echo -e "total script execution time is $TOTALTIME"
