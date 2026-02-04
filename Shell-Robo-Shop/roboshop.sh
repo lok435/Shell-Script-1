b96a4f51bf10536f183eea169f6271b86478f1e7
Loknath Reddy <devopsloknath94@gmail.com>
2026-02-04 20:41:40 +0530

    this is additional message

--- Shell-Robo-Shop/roboshop.sh
+++ Shell-Robo-Shop/roboshop.sh
@@ -5,11 +5,31 @@ INSTANCE_TYPE="t3.micro"
 SECURITY_GROUP_ID="sg-08c7f0d0c031b570c"
 #SUBNET_ID="subnet-xxxxxxxx"
 REGION="us-east-1"
+for instance in $@
+do 
+    INSTANCE_ID=$(aws ec2 run-instances \
+    --region $REGION \
+    --image-id $AMI_ID \
+    --instance-type $INSTANCE_TYPE \
+    --security-group-ids $SECURITY_GROUP_ID \
+    #--subnet-id $SUBNET_ID \
+    --tag-specifications 'ResourceType=instance,Tags={Key=Name,Value=$instance}' 
+    --query 'Reservations[].Instances[].PublicIpAddress' \
+        --output text )
 
-aws ec2 run-instances \
-  --region $REGION \
-  --image-id $AMI_ID \
-  --instance-type $INSTANCE_TYPE \
-  --security-group-ids $SECURITY_GROUP_ID \
-  #--subnet-id $SUBNET_ID \
-  --tag-specifications 'ResourceType=instance,Tags={Key=<TagKey>,Value=mongo}' 
+    if [ $instance == frontend ];then
+       IP=$(
+        aws ec2 describe-instances\
+         --instance-ids $INSTANCE_ID\
+         --query 'Reservations[0].Instances[0].PublicIpAddress'\
+         --output text
+       )
+        else
+        IP=$(
+        aws ec2 describe-instances\
+         --instance-ids $INSTANCE_ID\
+         --query 'Reservations[0].Instances[0].PrivateIpAddress'\
+         --output text
+        )
+    fi   
+done        
