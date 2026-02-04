4c4d7fa0c5815ef5cb68cdc880059fa0eab42ff8
Loknath Reddy <devopsloknath94@gmail.com>
2026-02-04 12:42:29 +0530

    modfied

--- trash.sh
+++ trash.sh
@@ -6,20 +6,13 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+set -e 
+trap 'echo "Error on line $LINENO with exit status $?"' ERR
 
 LOG_FOLDER="/var/log/Shell-script"
 LOG_FILE="/var/log/Shell-script/$0.log"
 
 mkdir -p /var/log/Shell-script &>> LOG_FILE
-
-validate(){
-    if [ $1 -ne 0 ];then 
-    echo -e "$2..$R failed $N"
-    else 
-    echo -e "$2..$G success $N"
-    fi
-}
-
 if [ $ID -ne 0 ];then 
 echo -e " $R..please run this script with root user $N"
 exit 1
@@ -30,15 +23,14 @@ do
     yum list installed $package &>> $LOG_FILE
     if [ $? -ne 0 ];then 
     yum install $package -y  &>> $LOG_FILE
-    validate $? "installing nginx"
+    #validate $? "installing nginx"
     else
     echo -e "$package..is already installed $Y skipping$N"
     fi
 done
 echo "this script executed at $TIMESTAMP"
-set -e
 echo "this line is for demonstrating"
 sleep 11
 ENDTIME=$(date +%s)
 TOTALTIME=$(($ENDTIME-$STARTTIME))
-echo -e "total script execution time is $TOTALTIME"
+echo -e "total script execution time is $TOTALTIME
