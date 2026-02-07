343e43eefd049efb5e0e81ac46062b4b4e0ed331
Loknath Reddy <devopsloknath94@gmail.com>
2026-02-07 19:58:35 +0530

    updating records

---
+++ Shell-Robo-Shop/mongodb.sh
@@ -0,0 +1,43 @@
+STARTTIME=$(date +%s)
+TIMESTAMP=$(date)
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOG_FOLDER="/var/log/Shell-script"
+LOG_FILE="$LOG_FOLDER/$0.log"
+
+mkdir -p /var/log/Shell-script &>> LOG_FILE
+
+validate(){
+    if [ $1 -ne 0 ];then 
+    echo -e "$2..$R failed $N" | tee -a $LOG_FILE
+    else 
+    echo -e "$2..$G success $N" | tee -a $LOG_FILE
+    fi
+}
+
+if [ $ID -ne 0 ]; then 
+echo -e " $R..please run this script with root user $N"
+exit 1
+fi
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+validate $? "copying mongo.repo"
+
+dnf install mongodb-org -y 
+validate $? "installing mongodb"
+
+systemctl enable mongod
+validate $? "enabling mongodb"
+
+systemctl start mongod
+validate $? "staring mongodb"
+
+sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf
+validate $? "changing the port"
+
+systemctl restart mongod 
+validate $? "restarting MONGODB"
