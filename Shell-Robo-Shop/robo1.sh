ee8816bbe33e4d418faac2069a61bd021f864f9b
Loknath Reddy <devopsloknath94@gmail.com>
2026-02-07 18:26:23 +0530

    updating records

--- Shell-Robo-Shop/robo1.sh
+++ Shell-Robo-Shop/robo1.sh
@@ -1,11 +1,14 @@
+SG_ID=$sg-08c7f0d0c031b570c
+AIM_ID=$"ami-0220d79f3f480ecf5"
 HOSTEDZONE_Id=$"Z007932124TF6WJ9OLE5"
 DOMAIN_NAME=$"frontend.bnbs.life"
+
 for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances \
-  --image-id ami-0220d79f3f480ecf5 \
+  --image-id=$AMI_ID \
   --instance-type t3.micro \
-  --security-group-ids sg-08c7f0d0c031b570c \
+  --security-group-ids=$SG_ID \
   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
   --query 'Instances[0].InstanceId' \
   --output text)
